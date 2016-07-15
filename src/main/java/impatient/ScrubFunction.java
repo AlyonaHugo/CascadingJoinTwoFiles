ed12d14b1d22d0c44afb69b8c9f86b198196ba2b
AlyonaHugo <Alonyshka_eromyshka@ukr.net>
2016-07-15 19:12:27 +0300

    Update ScrubFunction.java
    improve comment

--- src/main/java/impatient/ScrubFunction.java
+++ src/main/java/impatient/ScrubFunction.java
@@ -19,7 +19,7 @@ public class ScrubFunction extends BaseOperation implements Function
     {
         TupleEntry argument = functionCall.getArguments();
         String interface1 = argument.getString( 0 );
-        System.out.println("interface " + interface1 );
+        System.out.println("channel " + interface1 );
         Integer amount = new Integer(argument.getString( 1 ));
         System.out.println("amount " + amount);
         Tuple result = new Tuple();
